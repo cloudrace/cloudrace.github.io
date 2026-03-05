0eb874af75faf29a1becf80cc87796aec0c4a03d
kanch <kanch@cloudrace.info>
2026-03-04 21:04:45 -0500

    Fixed deployment script and baseURL

--- deploy.sh
+++ deploy.sh
@@ -11,31 +11,12 @@ if [ -n "$*" ]; then
 fi  
 
 rm -rf docs
-hugo 
+hugo --gc --cleanDestinationDir
 cp CNAME docs/
 #force add all files when needed
-
-# Prompt the user for their Git username
-read -p "Enter your Git username: " git_username
-
-# Validate the username (optional)
-if [[ -z "$git_username" ]]; then
-  echo "Error: Git username cannot be empty."
-  exit 1
-fi
 
 #git add -A
 git add .
 git commit -m "$msg"
 #--force when you need to force your push
 git push origin main
-
-
-
-# You can add more validation checks here, such as:
-# - Checking if the username exists in a specific list
-# - Enforcing a specific format for the username
-
-# Now you can use the 'git_username' variable to push content
-# For example:
-git push origin main --author="$git_username"
